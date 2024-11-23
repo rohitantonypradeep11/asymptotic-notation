075afd5af89070f59a3a6e47a26bb3a32040b99c
rohitantonypradeep <rohitantonypradeep!@gmail.com>
2024-11-23 12:55:05 +0000

    an

---
+++ o.py
@@ -0,0 +1,7 @@
+def abc(n):
+    i = 0
+    for a in range(1,n+1):
+        i = i+1
+    print(n,i)
+abc(10)
+abc(2323)
---
+++ on.py
@@ -0,0 +1,9 @@
+def abc(n):
+    a = 0
+    for i in range(0,n):
+        for j in range(0,n):
+            print("*",end = " ")
+        print("")
+    print(a,n)
+abc(4)
+abc(20)
